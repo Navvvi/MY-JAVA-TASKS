27458f753be2996547a3135b7175f2c1700a9c31
Navvvi <navyagunasekaran1995@gmail.com>
2016-11-14 15:47:46 +0530

    Create Rectangle.java

---
+++ SHAPES/SOURCE_FILES/Rectangle.java
@@ -0,0 +1,11 @@
+package com;
+public class Rectangle {
+	public double length;
+	public double breadth;
+	public void Area()
+	{
+           double a= length* breadth;
+           System.out.println("Area of rectangle is:"+a);
+	}
+
+}
