c4d0eaf7641f11805763d4165e81689cea6e74f8
Navvvi <navyagunasekaran1995@gmail.com>
2016-11-14 15:48:19 +0530

    Create Triangle.java

---
+++ SHAPES/SOURCE_FILES/Triangle.java
@@ -0,0 +1,10 @@
+package com;
+public class Triangle {
+	public double base;
+	public double height;
+	public void Area()
+	{
+		double a= (0.5)*base * height;
+		System.out.println("Area of rectangle is :"+a);
+	}
+}
